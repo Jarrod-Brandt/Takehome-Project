e5fee46cd90d6d72b7782c5173e99901c56c664e
Jarrod Brandt <jbkraut64@gmail.com>
2024-01-18 11:41:43 -0800

    structural changes
    a mock up of the structure of a theoretical tool suite

---
+++ src/core classes/environments/3body.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 18 11:31:40 2024
+
+@author: jbkra
+"""
+
---
+++ src/core classes/environments/other models (atmosphere, srp ect).py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 18 11:39:27 2024
+
+@author: jbkra
+"""
+
